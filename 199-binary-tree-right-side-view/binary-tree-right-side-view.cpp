b70dfe9f2aef0056a53e877d7fc6c4f379db237e
Ehtesham <mahcazer@gmail.com>
2025-08-12 19:13:18 +0530

    Time: 3 ms (7.13%) | Memory: 14.9 MB (61.07%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -11,28 +11,48 @@
  */
 class Solution {
 public:
-    vector <int > bfs (TreeNode* root)
-    {  vector <int > ans ;
-      if(!root)return {};
-        queue<TreeNode*> c;
-        c.push(root);
-        while(!c.empty())
-        {
-            int n=c.size();
-            vector<int> lvl;
-            for (int i=0;i<n;++i)
-            {   
-               TreeNode* node=c.front();
-               c.pop();
-               lvl.push_back(node->val);
-               if (node->left!=NULL)c.push(node->left);
-               if (node->right!=NULL)c.push(node->right);
-            }
-             ans.push_back(lvl[lvl.size()-1]);
-        }
-       return ans ;
+    void solve (TreeNode* root,int lvl,vector <int> &ans)
+    { 
+     if (!root) return ;
+    if (lvl==ans.size())
+    {
+        ans.push_back(root->val);
+    }
+     // go the right
+     solve(root->right,lvl+1,ans);       
+     solve(root->left,lvl+1,ans);   
+
+       
     }
     vector<int> rightSideView(TreeNode* root) {
-         return bfs(root);
+        vector<int> ans;
+         solve(root,0,ans);
+         return ans;
     }
 };
+
+
+//BFS Approach:
+
+// vector <int > bfs (TreeNode* root)
+//     { 
+//          vector <int > ans ;
+//       if(!root)return {};
+//         queue<TreeNode*> c;
+//         c.push(root);
+//         while(!c.empty())
+//         {
+//             int n=c.size();
+//             vector<int> lvl;
+//             for (int i=0;i<n;++i)
+//             {   
+//                TreeNode* node=c.front();
+//                c.pop();
+//                lvl.push_back(node->val);
+//                if (node->left!=NULL)c.push(node->left);
+//                if (node->right!=NULL)c.push(node->right);
+//             }
+//              ans.push_back(lvl[lvl.size()-1]);
+//         }
+//        return ans ;
+//     }
