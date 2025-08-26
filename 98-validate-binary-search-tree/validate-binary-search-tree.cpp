a1cc19965cf61d3ac50f96c38852d5ae9af1c5ba
Ehtesham <mahcazer@gmail.com>
2025-08-26 23:17:27 +0530

    Time: 0 ms (100.00%) | Memory: 21.8 MB (75.38%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/validate-binary-search-tree.cpp
@@ -11,33 +11,46 @@
  */
 class Solution {
 public:
-     void inorder(TreeNode* root,vector<int> &a)
-     {
-       if (!root)return;
+    //  void inorder(TreeNode* root,vector<int> &a)
+    //  {
+    //    if (!root)return;
 
-       inorder(root->left,a);
-       a.push_back(root->val);
-       inorder(root->right,a);
-     }
+    //    inorder(root->left,a);
+    //    a.push_back(root->val);
+    //    inorder(root->right,a);
+    //  }
+
+    bool check(TreeNode* root,long long min,long long max)
+    {  if (!root)return true;
+
+        if (root->val<=min || root->val>=max)return false;
+        bool left=check(root->left,min,root->val);
+        bool right=check(root->right,root->val,max);
+        return (left) && (right);
+
+    }
+
     bool isValidBST(TreeNode* root) {
-        vector <int> ans;
-        inorder(root,ans);
-        if (ans.size()<=1)return true;
-        // no repetition 
-        unordered_map<int,int> m ;
-        for ( int x:ans)
-        {
-            m[x]++;
-        }
-        for (auto it:m)
-        {
-            if (it.second>1)return false;
-        }
-        if (ans[0]==ans[ans.size()-1])return false;
-        vector<int> temp=ans;
+        // vector <int> ans;
+        // inorder(root,ans);
+        // if (ans.size()<=1)return true;
+        // // no repetition 
+        // unordered_map<int,int> m ;
+        // for ( int x:ans)
+        // {
+        //     m[x]++;
+        // }
+        // for (auto it:m)
+        // {
+        //     if (it.second>1)return false;
+        // }
+        // if (ans[0]==ans[ans.size()-1])return false;
+        // vector<int> temp=ans;
 
-        sort(temp.begin(),temp.end());
-        if (temp==ans)return true ;
-        else return false;
+        // sort(temp.begin(),temp.end());
+        // if (temp==ans)return true ;
+        // else return false;
+
+        return check(root,LLONG_MIN,LLONG_MAX);
     }
 };
