fb4198f12e630795f422900f5ea33a20ff9a153e
Ehtesham <mahcazer@gmail.com>
2025-09-13 01:15:54 +0530

    Time: 11 ms (34.01%) | Memory: 19 MB (54.04%) - LeetSync

---
+++ 3462-vowels-game-in-a-string/vowels-game-in-a-string.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool doesAliceWin(string s) {
+        int vc=0;
+        for (char c: s)
+        {
+             if (c=='a'||c=='e'||c=='i'||c=='o'||c=='u')
+             {
+               vc++;
+             }
+        }
+
+        if (vc==0)return false;
+        else return true;
+    }
+};
