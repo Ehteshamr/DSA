a5e950217cbf1b2ffacb443090818e0db43712aa
Ehtesham <mahcazer@gmail.com>
2026-05-01 18:59:24 +0530

    Time: 2 ms (90.99%) | Memory: 8.5 MB (77.16%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,50 +1,26 @@
 class Solution {
-public: 
-//    bool check(string s)
-//    {
-//        string a=s;
-//        reverse(a.begin(),a.end());
-//        if (a==s)return true;
-//        else return false;
-//    }
-
-  int count(string s, int i,int j)
-  {  int count=0;
-     while(i>=0 && j<s.size() && s[i]==s[j])
-     {
-        count++;
-        i--;
-        j++;
-     }
-     return count;
-  }
+public:
     int countSubstrings(string s) {
-        int c=0;
-       
-        for (int i=0;i<s.size();++i)
-        {  
-            int odd=count(s,i,i);
-          int even=count(s,i,i+1);
-           
-           c+=odd+even;
-          
+        // expand around center method 
+        int n=s.size();int count=0;
+        for ( int i=0;i<n;++i)
+        {
+            // odd length 
+            int l=i,r=i;
+            while (l>=0 && r<n && s[l]==s[r])
+            {
+               count++;
+               l--; r++;
+            }
+  
+   // even length
+            l=i;r=i+1;
+            while (l>=0 && r<n && s[l]==s[r])
+            {
+               count++;
+               l--; r++;
+            }
         }
-
-        return c;
+        return count;
     }
 };
-
-
-//   for (int i=0;i<s.size();++i)
-//         {   
-//             string temp="";
-//             temp+=s[i];
-//             if (check(temp))count++;
-        
-//           for ( int j=i+1;j<s.size();++j)
-//           {
-//             temp+=s[j];
-//             if (check(temp))count++;
-//           }
-//            temp="";
-//         }
